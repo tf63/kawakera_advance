bcb10eaa3b29865d2079af008ae9f2476e438e72
tf63 <tfukuda9031@gmail.com>
2023-06-12 16:00:53 +0900

    fix: add home card

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import Card from './Card'
-import LinkCard from './LinkCard'
 
 function Home() {
     return (
         <div>
-            <LinkCard content="About" link="/about" color="Green" />
-            <LinkCard content="Profile" link="/profile" color="Green" />
-            <LinkCard content="Select" link="/select" color="Green" />
-            <Card content="Card" />
-            <Card content="Card" />
-            <Card content="Card" />
-            <Card content="Card" />
+            <div className="card">Home</div>
         </div>
     )
 }
