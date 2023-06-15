88c737e5cfca82c29b5bde59703611072ac01775
tf63 <tfukuda9031@gmail.com>
2023-06-15 16:33:53 +0900

    feat: adjust css

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,16 +3,22 @@ import { Link } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
     return (
-        <nav style={{ height: '50px', display: 'flex', alignItems: 'center' }}>
-            <ul style={{ display: 'flex', listStyle: 'none', padding: '0', margin: '0' }}>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="/">Home</Link>
+        <nav className="card">
+            <ul>
+                <li>
+                    <Link className="link" style={{ color: 'inherit' }} to="/">
+                        Home
+                    </Link>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="#">About</Link>
+                <li>
+                    <Link className="link" style={{ color: 'inherit' }} to="#">
+                        About
+                    </Link>
                 </li>
-                <li style={{ marginRight: '10px' }}>
-                    <Link to="#">Contact</Link>
+                <li>
+                    <Link className="link" style={{ color: 'inherit' }} to="#">
+                        Contact
+                    </Link>
                 </li>
             </ul>
         </nav>
