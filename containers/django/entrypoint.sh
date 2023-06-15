d24bcc6b2c616dda0804966293bf9d12e81a6f2a
tf63 <tfukuda9031@gmail.com>
2023-06-15 11:56:41 +0900

    feat: add fixture

--- containers/django/entrypoint.sh
+++ containers/django/entrypoint.sh
@@ -2,7 +2,7 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py loaddata api/fixtures/problem_data.json
+python manage.py loaddata api/fixtures/*.json
 
 # 環境変数のDEBUGの値がTrueの時はrunserverを、Falseの時はgunicornを実行します
 # シェルスクリプトでは`[`と`$DEBUG`、`1`と`]`の間にスペースを一つ空けておかないと[]内の式を認識できないので注意
