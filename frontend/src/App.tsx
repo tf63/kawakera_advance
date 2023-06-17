85cafcf779020cce0cd2261336da8b0bdfd74717
tf63 <tfukuda9031@gmail.com>
2023-06-17 16:21:53 +0900

    feat: add tutorial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,15 +6,24 @@ import Home from './components/Home'
 import Detail from './components/Detail'
 import './App.css'
 import Navbar from './components/Navbar'
+import BallAnimation from './components/Animation/Rolling'
+import BoxAnimation from './components/Animation/Box'
+import ThrowAnimation from './components/Animation/Throw'
+import SlotMachine from './components/Animation/Slot'
 
 function App() {
     return (
         <Router>
-            <Navbar />
+            {/* <Navbar /> */}
             <Routes>
                 {/* Nav */}
                 <Route path="/" element={<Home />} />
                 <Route path="/detail/:id" element={<Detail />} />
+                <Route path="/detail" element={<Detail />} />
+                <Route path="/rolling" element={<BallAnimation />} />
+                <Route path="/box" element={<BoxAnimation />} />
+                <Route path="/throw" element={<ThrowAnimation />} />
+                <Route path="/slot" element={<SlotMachine />} />
             </Routes>
         </Router>
     )
